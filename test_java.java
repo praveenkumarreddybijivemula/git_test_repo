80079aa14bc6bf19205ac1900ea6e02a71bca599
praveenkumarreddybijivemula <58033195+praveenkumarreddybijivemula@users.noreply.github.com>
2019-12-16 13:37:55 +0530

    Update test_java.java

--- test_java.java
+++ test_java.java
@@ -1,5 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Praveen Reddy");  
+     System.out.println("Hello Praveen Reddy");
+     System.out.println("Hello Pranavi Reddy");  
     }  
 }  
